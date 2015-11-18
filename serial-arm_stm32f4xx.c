b9538bcb5b37c944358a10c8d9e40b0402f7a5fa
wurstnase <wurstnase.reprap@gmail.com>
2015-11-19 00:21:31 +0100

    STM32F411: serial-arm_stm32f4xx.c: allow sending arbitrarily long messages.
    On ARM we use only the 16 byte hardware buffer for sending and
    receiving over the serial line, which is often too short for
    debugging messages. This implementation works fine and still
    neither blocks nor introduces delays for short messages.
    
    Removed while-loop. Looks like we need some more us than the LPC?!? With +7us
    we do not lose characters anymore.

--- serial-arm_stm32f4xx.c
+++ serial-arm_stm32f4xx.c
@@ -11,7 +11,9 @@
 */
 
 #if defined TEACUP_C_INCLUDE && defined __ARM_STM32F411__
+
 #include "arduino.h"
+#include "delay.h"
 
 #ifdef XONXOFF
   #error XON/XOFF protocol not yet implemented for ARM. \
@@ -83,7 +85,9 @@ void serial_init()
     Divisor = int((div - int(div))*16)
     BRR = Mantisse + Divisor
     */
+    #if !defined BAUD
     #define BAUD 115200
+    #endif
 
     #if (BAUD == 115200)
         USART2->BRR = (uint32_t)(0x01A1);
@@ -131,7 +135,8 @@ uint8_t serial_txchars(void) {
 /** Send one character.
 */
 void serial_writechar(uint8_t data) {
-  while (!serial_txchars());
+  if ( !serial_txchars())       // Queue full?
+    delay_us((1000000 / BAUD * 10) + 7);
   USART2->DR = (uint32_t)(data & 0x1FF);
 }
 
